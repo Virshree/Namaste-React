20d53f7217a85654589229909484030c3355692f
Virshree <virshreedesai19@gmail.com>
2024-04-05 17:33:57 +0530

    Igniting my app

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-/**
- * Nested element
- * <div id="parent">
- *      <div id="child">
- *      <h1></h1>
- *      <h2></h2>
- *      </div>
- *   <div id="child2">
- *      <h1></h1>
- *      <h2></h2>
- *      </div>
- * </div>
- * 
- * 
- */
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
 
 const parent=React.createElement("div",{id:"parent"},
 [React.createElement("div"
 ,{id:"child"},
-[React.createElement("h1",{id:"heading"},"I'm a h1 tag",
-React.createElement("h2",{id:"heading"},"I'm a h2 tag"))
+[React.createElement("h1",{id:"heading"},"Namaste React 🚀"  ,
+React.createElement("h2",{id:"heading"},"Virshree 💘"  ))
 ,
 React.createElement("div"
 ,{id:"child2"},
